b723fe6df96f54883c6df085c8f5646378ad6078
Jose Fernandez <bernardo.fersan@gmail.com>
2023-02-20 13:09:29 -0600

    added home page and css styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { Profile } from './PortfolioContainer/Home/Profile'
 
 function App() {
   
   return (
     <div className="App">
 
-      <div>
-        Hello World!
-      </div>
+      <Profile  />
       
     </div>
   )
---
+++ src/PortfolioContainer/Home/Profile.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import Typical from 'react-typical';
+import './profile.css';
+
+
+export const Profile = () => {
+  return (
+
+    <div className='profile-container' >
+        <div className='profile-parent' >
+            <div className='profile-details' >
+                <div className='colz' >
+                    <div className='colz-icon' >
+
+                        <a href="https://www.facebook.com/coco.fernandez.330">
+                            <i className='fa fa-facebook-square' ></i>
+                        </a>
+                        
+                        <a href="">
+                            <i className='fa fa-google-plus-square' ></i>
+                        </a>
+                        
+                        <a href="">
+                            <i className='fa fa-instagram' ></i>
+                        </a>
+                        
+                        <a href="">
+                            <i className='fa fa-youtube-square' ></i>
+                        </a>
+
+                        <a href="">
+                            <i className='fa fa-twitter' ></i>
+                        </a>
+
+                    </div>
+                </div>            
+
+            <div className='profile-details-name' >
+                <span className='primary-text' >
+                    { "" }
+                    HELLO, I'M <span className='highlighted-text' > JOSE </span>
+                </span>
+            </div>
+                <div className='profile-details-role' >
+                    <span className='primary-yext' >
+                        { "" }
+                        <h1>
+                            {" "}
+                            <Typical
+                            loop={ Infinity }
+                            steps={[
+                                "Passionate Developer ❤️ ",
+                                1000,
+                                "Full Stack Developer 💻 ",
+                                1000,
+                                "Problem Solver 🧠 ",
+                                1000,
+                                "JS, React, Java Developer 😎 ",
+                                1000,
+                                "Crypto Enthusiast 🌐 ",
+                                1000,
+                            ]}                            
+                            />
+                        </h1>
+                        <span className='profile-role-tagline' >
+                            Knack of building applications with front and back end operations.
+                        </span>
+                    </span>
+                </div>
+                <div className='profile-options' >
+                    <button className='btn primary-btn' >
+                        {""}
+                        Hire Me{" "}
+                    </button>
+                    <a href="JoseBernardoCV.pdf" download="joseCV.pdf" >
+                        <button className='btn highlighted-btn' > Get Resume </button>
+                    </a>
+                </div>
+            </div>
+            <div className='profie-picture' >
+                <div className='profile-picture-background' >
+
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
